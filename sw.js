da519049abf28f4e6b8bbec55bdc136a4b61e55d
filtry2018-afk <filtry2018@gmail.com>
2026-08-31 04:56:56 -0300

    Refresh PWA service worker cache v106

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='vv-trace360-105';
-const CORE=['./','./index.html','./styles.css?v=102','./app.js?v=102','./logo-loader.js?v=102','./manifest.webmanifest?v=105','./assets/icon-user-192.jpg?v=105','./assets/icon-user-512.jpg?v=105','./logo/part01.txt','./logo/part02.txt','./logo/part03.txt','./logo/part04.txt','./logo/part05.txt','./logo/part06.txt','./logo/part07.txt','./logo/part08.txt','./logo/part09.txt'];
+const CACHE='vv-trace360-106';
+const CORE=['./','./index.html','./styles.css?v=102','./app.js?v=106','./logo-loader.js?v=102','./manifest.webmanifest?v=106','./assets/icon-user-192.jpg?v=106','./assets/icon-user-512.jpg?v=106','./logo/part01.txt','./logo/part02.txt','./logo/part03.txt','./logo/part04.txt','./logo/part05.txt','./logo/part06.txt','./logo/part07.txt','./logo/part08.txt','./logo/part09.txt'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));return}e.respondWith(fetch(e.request,{cache:'no-store'}).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return resp}).catch(()=>caches.match(e.request))) });
